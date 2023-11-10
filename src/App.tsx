8bea88994bb8d6fd98ab0ebaa44d8c7782d22003
jlamar27 <jlamar2712@gmail.com>
2023-11-10 01:35:13 -0500

    accordion works

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar';
-import HomeIndex from './components/pages/Home/HomeIndex';
+import HomeIndex from './pages/Home/HomeIndex';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <HomeIndex/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/pages/Home/HomeIndex.tsx
@@ -0,0 +1,15 @@
+import Hero from '../../components/Hero'
+import WhoWeAre from '../../components/WhoWeAre'
+import WhyGrappling from '../../components/WhyGrappling'
+import FAQ from '../../components/FAQ'
+
+export default function index() {
+  return (
+    <>
+        <Hero />
+        <WhoWeAre />
+        <WhyGrappling />
+        <FAQ />
+    </>
+  )
+}
